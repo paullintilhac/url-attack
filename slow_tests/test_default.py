e50c70b11d9bfd0a0266380e39b74f110ebed235
ProfessorChaos <paul.lintilhac@gmail.com>
2023-04-04 00:56:01 -0400

    changes

--- slow_tests/test_default.py
+++ slow_tests/test_default.py
@@ -1,4 +1,12 @@
-import sys, os, datasets, time
+import sys, os, datasets, time, ssl
+ssl._create_default_https_context = ssl._create_unverified_context
+import torch
+import torch.nn as nn
+from torch.autograd import Variable
+from torch.utils.data import DataLoader
+import tensorflow.compat.v1 as tf
+tf.disable_v2_behavior()
+
 sys.path.insert(0, os.path.join(
     os.path.dirname(os.path.abspath(__file__)),
     ".."
@@ -6,6 +14,56 @@ sys.path.insert(0, os.path.join(
 
 import OpenAttack
 from attackers import get_attackers
+
+class RNNModel(nn.Module,OpenAttack.Classifier):
+    def __init__(self,input_dim,hidden_dim, output_dim,num_layers):
+        super(RNNModel, self).__init__()
+        # Number of hidden dimension
+        self.output_dim = output_dim
+        self.hidden_dim = hidden_dim
+        # Number of hidden layers
+        self.num_layers = num_layers
+
+        self.input_dim=input_dim
+
+        # RNN
+        self.rnn = nn.RNN(input_dim, hidden_dim, num_layers, batch_first=True,bidirectional=False, nonlinearity='relu')
+        #self.rnn = nn.GRU(input_dim, hidden_dim, num_layers, bidirectional=True, batch_first=True)
+
+        # Readout layer
+        self.fc = nn.Linear(hidden_dim, output_dim)
+        self.softmax = nn.LogSoftmax(dim=1)
+
+
+    def forward(self, x):
+
+        h0 = Variable(torch.zeros(self.num_layers, x.size(0), self.hidden_dim))
+        print("h0 size: "+ str(h0.size())+", x size: " + str(x.size()))
+
+        # One time step
+        #print("padded_x size: " + str(padded_x.size()))
+        out, hn = self.rnn(x, h0)
+        temp = self.fc(out[:, -1, :])
+        out = self.softmax(temp)
+        return out
+
+    def initHidden(self):
+        return torch.zeros(1,self.hidden_dim)
+
+    # access to the classification probability scores with respect input sentences
+    def get_prob(self, input_):
+        print("input size: " + str(input_.size()))
+        ret = []
+        for sent in input_: 
+            thing =  self.forward(input_)
+            ret.append(thing)        
+        # The get_prob method finally returns a np.ndarray of shape (len(input_), 2). See Classifier for detail.
+        return ret
+
+    def get_pred(self, input_):
+        thing = self.get_prob(input_)
+        print("thing: " + str(thing))
+        return torch.max(thing,1)[1]
 
 def dataset_mapping(x):
     return {
@@ -17,16 +75,33 @@ def main():
     import multiprocessing
     if multiprocessing.get_start_method() != "spawn":
         multiprocessing.set_start_method("spawn", force=True)
-    dataset = datasets.load_dataset("sst", split="train[:100]").map(function=dataset_mapping)
-    clsf = OpenAttack.loadVictim("BERT.SST") # .to("cuda:0")
 
-    attackers = get_attackers(dataset, clsf)
+    x_test = torch.load('../Adversarial-Attack-on-recurrent-neural-network/x_test_tomita6.pt').numpy()
+    y_test = torch.load('../Adversarial-Attack-on-recurrent-neural-network/y_test_tomita6.pt').numpy()
+    x_train = torch.load('../Adversarial-Attack-on-recurrent-neural-network/x_train_tomita6.pt').numpy()
+    y_train = torch.load('../Adversarial-Attack-on-recurrent-neural-network/y_train_tomita6.pt').numpy()
+    dataset = []
+    for i in range(len(x_test)):
+        this_dict = {'x': list(x_test[i]),'y': int(y_test[i])}
+        dataset.append(this_dict)
+    
+    # dataset = datasets.load_dataset("sst", split="train[:100]").map(function=dataset_mapping)
 
+    def load_checkpoint(filepath):
+        checkpoint = torch.load(filepath)
+        model = RNNModel(2,100,2,1)
+        model.load_state_dict(checkpoint['state_dict'])
+        return model
+    
+    model = load_checkpoint('./rnn model tomita 6.pth')
+
+    attackers = get_attackers(dataset, model)
+    print("attackers: " + str(attackers))
     for attacker in attackers:
         print(attacker.__class__.__name__)
         try:
             print(
-                OpenAttack.AttackEval(attacker, clsf).eval(dataset, progress_bar=True),
+                OpenAttack.AttackEval(attacker, model).eval(dataset, progress_bar=True),
             )
         except Exception as e:
             raise e
